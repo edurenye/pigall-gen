b9b97f10dc960a520deb58e5e456d98064e19371
edurenye <edurenye@hotmail.com>
2020-07-03 00:43:34 +0200

    Compile OpenCV with OpenVINO

--- pigall/02-setup-pigall/00-run.sh
+++ pigall/02-setup-pigall/00-run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -e
 
+# OpenVINO
+# https://docs.openvinotoolkit.org/2020.3/_docs_install_guides_installing_openvino_raspbian.html
+# https://github.com/opencv/opencv/wiki/Intel's-Deep-Learning-Inference-Engine-backend#raspbian-buster
+mkdir -p ${ROOTFS_DIR}/opt/intel
+cp -r files/openvino ${ROOTFS_DIR}/opt/intel/
+pushd ${ROOTFS_DIR}/opt/intel/openvino
+on_chroot << EOF
+source /opt/intel/openvino/bin/setupvars.sh
+usermod -a -G users "pi"
+sh /opt/intel/openvino/install_dependencies/install_NCS_udev_rules.sh
+EOF
+popd
+
 #
 # Download sources
 #
@@ -28,30 +41,39 @@ on_chroot << EOF
 pip3 install numpy
 EOF
 
+#
+# Build
+#
 pushd ${STAGE_WORK_DIR}
 on_chroot << EOF
 mkdir build
 pushd build
 cmake "/usr/src/opencv-4.3.0" \
-    -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_BUILD_TYPE=Release \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D ENABLE_NEON=ON \
-    -D ENABLE_VFPV3=ON \
+    -D WITH_IPP=OFF \
     -D BUILD_TESTS=OFF \
+    -D BUILD_PERF_TESTS=OFF \
+    -D OPENCV_ENABLE_PKG_CONFIG=ON \
+    -D PKG_CONFIG_EXECUTABLE="/usr/bin/arm-linux-gnueabihf-pkg-config" \
+    -D ENABLE_NEON=ON \
+    -D CPU_BASELINE="NEON" \
+    -D ENABLE_VFPV3=ON \
     -D INSTALL_PYTHON_EXAMPLES=OFF \
     -D OPENCV_ENABLE_NONFREE=ON \
     -D CMAKE_SHARED_LINKER_FLAGS=-latomic \
-    -D BUILD_EXAMPLES=OFF ..
+    -D BUILD_EXAMPLES=OFF \
+    -D WITH_INF_ENGINE=ON \
+    -D INF_ENGINE_LIB_DIRS="/opt/intel/openvino/inference_engine/lib/armv7l" \
+    -D INF_ENGINE_INCLUDE_DIRS="/opt/intel/openvino/inference_engine/include" \
+    -D CMAKE_FIND_ROOT_PATH="/opt/intel/openvino" \
+    -D ENABLE_CXX11=ON ..
 make -j4
 make install
 ldconfig
 popd
 EOF
 popd
-
-#
-# Build
-#
 
 
 # Server.
